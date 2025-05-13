88e47acedf1617b3453f7ea00b78d2fdf519983d
bondiano <batbondik0@gmail.com>
2025-05-13 18:24:18 +0500

    implement option and list functions

--- src/main/scala/ru/otus/Main.scala
+++ src/main/scala/ru/otus/Main.scala
@@ -1,11 +1,13 @@
 package ru.otus
 
-import ru.otus.module1.{hof, type_system}
+import ru.otus.module1.{hof, type_system, list, opt}
 
 
 object App {
   def main(args: Array[String]): Unit = {
     trait A extends Serializable
     object A
+
+    list.test
   }
 }
