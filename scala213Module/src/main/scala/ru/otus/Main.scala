9407a9800f12caae9b90456535e539dbc00c3344
bondiano <batbondik0@gmail.com>
2025-06-29 23:41:38 +0700

    add cats typeclasses

--- scala213Module/src/main/scala/ru/otus/Main.scala
+++ scala213Module/src/main/scala/ru/otus/Main.scala
@@ -6,12 +6,17 @@ import ru.otus.module2.{catsTypeClasses, functional, implicits, type_classes, va
 import ru.otus.module3.functional_effects.functionalProgram.{declarativeEncoding, executableEncoding}
 
 import scala.util.{Failure, Success}
+import ru.otus.module2.catsHomework._
+import cats.implicits._
 
 
 object Main {
 
 
   def main(args: Array[String]): Unit = {
-    declarativeEncoding.run(declarativeEncoding.greet2)
+    val tree: Tree[Int] = Branch(Leaf(1), Leaf(2))
+    println(tree.map(x => x * 2))
+
+    // declarativeEncoding.run(declarativeEncoding.greet2)
   }
-} 
+}
