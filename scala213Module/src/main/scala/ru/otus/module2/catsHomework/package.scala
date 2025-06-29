94f79c29cbc2eca1763857147388dee6d4f138d4
bondiano <batbondik0@gmail.com>
2025-06-29 23:41:38 +0700

    add cats typeclasses

--- scala213Module/src/main/scala/ru/otus/module2/catsHomework/package.scala
+++ scala213Module/src/main/scala/ru/otus/module2/catsHomework/package.scala
@@ -1,5 +1,9 @@
 package ru.otus.module2
 
+import cats.implicits._
+import cats.Functor
+import scala.util.{Try, Success, Failure}
+
 package object catsHomework {
 
   /**
@@ -16,7 +20,12 @@ package object catsHomework {
    * Проверьте, что код работает корректно для Branch и Leaf
    */
 
-   lazy val treeFunctor = ???
+   implicit val treeFunctor: Functor[Tree] = new Functor[Tree] {
+    override def map[A, B](fa: Tree[A])(f: A => B): Tree[B] = fa match {
+      case Branch(left, right) => Branch(map(left)(f), map(right)(f))
+      case Leaf(value) => Leaf(f(value))
+    }
+   }
 
   /**
    * Monad абстракция для последовательной
@@ -54,14 +63,46 @@ package object catsHomework {
    * Напишите instance MonadError для Try
    */
 
-   lazy val tryME = ???
+   implicit val tryME: MonadError[Try, Throwable] = new MonadError[Try, Throwable] {
+    override def flatMap[A, B](fa: Try[A])(f: A => Try[B]): Try[B] = fa.flatMap(f)
+    override def pure[A](v: A): Try[A] = Success(v)
+
+    override def raiseError[A](e: Throwable): Try[A] = Failure(e)
+    override def handleErrorWith[A](fa: Try[A])(f: Throwable => Try[A]): Try[A] = fa match {
+      case Success(value) => Success(value)
+      case Failure(e) => f(e)
+    }
+    override def handleError[A](fa: Try[A])(f: Throwable => A): Try[A] = fa match {
+      case Success(value) => Success(value)
+      case Failure(e) => Success(f(e))
+    }
+    override def ensure[A](fa: Try[A])(e: Throwable)(f: A => Boolean): Try[A] = fa match {
+      case Success(value) => if (f(value)) Success(value) else Failure(e)
+      case Failure(e) => Failure(e)
+    }
+   }
 
   /**
    * Напишите instance MonadError для Either,
    * где в качестве типа ошибки будет String
    */
 
-   val eitherME = ???
+  type EitherString[A] = Either[String, A]
+  implicit val eitherME: MonadError[EitherString, String] = new MonadError[EitherString, String] {
+    override def flatMap[A, B](fa: EitherString[A])(f: A => EitherString[B]): EitherString[B] = fa.flatMap(f)
+    override def pure[A](v: A): EitherString[A] = Right(v)
+
+    override def raiseError[A](e: String): EitherString[A] = Left(e)
+    override def handleErrorWith[A](fa: EitherString[A])(f: String => EitherString[A]): EitherString[A] = fa match {
+      case Left(e) => f(e)
+      case Right(value) => Right(value)
+    }
+    override def handleError[A](fa: EitherString[A])(f: String => A): EitherString[A] = fa match {
+      case Left(e) => Right(f(e))
+      case Right(value) => Right(value)
+    }
+    override def ensure[A](fa: EitherString[A])(e: String)(f: A => Boolean): EitherString[A] = fa.filterOrElse(f, e)
+  }
 
 
 }
